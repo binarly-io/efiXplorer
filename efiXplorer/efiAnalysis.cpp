6dc70ed1b95211b156c4064e88a28f9d796f75f3
yeggor <vasilenko.yegor@gmail.com>
2022-07-30 21:51:11 +0300

    Extract GUIDs from .text segments

--- efiXplorer/efiAnalysis.cpp
+++ efiXplorer/efiAnalysis.cpp
@@ -1733,11 +1733,15 @@ void EfiAnalysis::EfiAnalyzer::markInterfaces() {
 }
 
 //--------------------------------------------------------------------------
-// Mark GUIDs found in the .data segment
+// Mark GUIDs found in the .text and .data segment
 void EfiAnalysis::EfiAnalyzer::markDataGuids() {
-    for (auto seg : dataSegments) {
-        segment_t *s = seg;
-        msg("[%s] marking .data GUIDs from 0x%016llX to 0x%016llX\n", plugin_name,
+    auto guids_segments = textSegments;
+    // find GUIDs in .text and .data segments
+    // TODO: scan only the areas between the beginning of the .text segment and the first
+    // function address (?)
+    guids_segments.insert(guids_segments.end(), dataSegments.begin(), dataSegments.end());
+    for (auto s : guids_segments) {
+        msg("[%s] marking GUIDs from 0x%016llX to 0x%016llX\n", plugin_name,
             static_cast<uint64_t>(s->start_ea), static_cast<uint64_t>(s->end_ea));
         ea_t ea = s->start_ea;
         while (ea != BADADDR && ea <= s->end_ea - 15) {
