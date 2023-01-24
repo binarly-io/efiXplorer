884043a2f5573d0d8b67bd00bbdde54338ea04db
yeggor <vasilenko.yegor@gmail.com>
2023-01-24 04:52:23 +0400

    Update copyright

--- efiXplorer/efiAnalyzer.h
+++ efiXplorer/efiAnalyzer.h
@@ -1,6 +1,6 @@
 /*
  * efiXplorer
- * Copyright (C) 2020-2022 Binarly
+ * Copyright (C) 2020-2023 Binarly
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- efiXplorer/efiHexRays.h
+++ efiXplorer/efiHexRays.h
@@ -1,6 +1,6 @@
 /*
  * efiXplorer
- * Copyright (C) 2020-2022 Binarly, Rolf Rolles
+ * Copyright (C) 2020-2023 Binarly, Rolf Rolles
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
