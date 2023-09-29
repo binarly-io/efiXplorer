e0052b71d73cdc76c4d649afacd30ad20d2da426
yeggor <vasilenko.yegor@gmail.com>
2023-09-29 17:13:26 +0100

    fix plugin and loader build with xcode 15 tools: * add -Wl,-ld_classic to _ida_compile_options (https://developer.apple.com/documentation/xcode-release-notes/xcode-15-release-notes)

--- cmake/FindIdaSdk.cmake
+++ cmake/FindIdaSdk.cmake
@@ -121,8 +121,9 @@ function(_ida_plugin name ea64 link_script) # ARGN contains sources
   if(UNIX)
     target_compile_options(${t} PUBLIC ${_ida_compile_options})
     if(APPLE)
-      target_link_libraries(${t} ${_ida_compile_options} -Wl,-flat_namespace
-                            -Wl,-undefined,warning -Wl,-exported_symbol,_PLUGIN)
+      target_link_libraries(
+        ${t} ${_ida_compile_options} -Wl,-ld_classic -Wl,-flat_namespace
+        -Wl,-undefined,warning -Wl,-exported_symbol,_PLUGIN)
     else()
       # Always use the linker script needed for IDA.
       target_link_libraries(${t} ${_ida_compile_options} -Wl,--version-script
@@ -156,8 +157,9 @@ function(_ida_loader name ea64 link_script)
   if(UNIX)
     target_compile_options(${t} PUBLIC ${_ida_compile_options})
     if(APPLE)
-      target_link_libraries(${t} ${_ida_compile_options} -Wl,-flat_namespace
-                            -Wl,-undefined,warning -Wl,-exported_symbol,_LDSC)
+      target_link_libraries(
+        ${t} ${_ida_compile_options} -Wl,-ld_classic -Wl,-flat_namespace
+        -Wl,-undefined,warning -Wl,-exported_symbol,_LDSC)
     else()
       # Always use the linker script needed for IDA.
       target_link_libraries(${t} ${_ida_compile_options} -Wl,--version-script
