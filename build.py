05a7dd83651bee937a27c640f7a89de48c8cd53f
Nikolaj Schlej <nschlej@apple.com>
2023-12-20 02:50:25 +0000

    add ability to build plugin and loader in one action (#84)

--- build.py
+++ build.py
@@ -64,8 +64,34 @@ def build_loader(idasdk: str):
     subprocess.call(["cmake", "--build", ".", "--config", "Release", "--parallel"])
 
 
+@click.command()
+@click.option(
+    "--hexrays_sdk",
+    "hexrays_sdk",
+    type=str,
+    default=str(),
+    help="path to hexrays_sdk directory",
+)
+@click.argument("idasdk")
+def build_everything(idasdk: str, hexrays_sdk: str):
+    """Build plugin (efiXplorer) and loader (efiXloader)"""
+
+    if not os.path.isdir("build"):
+        os.mkdir("build")
+
+    os.chdir("build")
+
+    command = ["cmake", "..", f"-DIdaSdk_ROOT_DIR={idasdk}"]
+    if hexrays_sdk:
+        print("[INFO] HexRays analysis will be enabled")
+        command.append(f"-DHexRaysSdk_ROOT_DIR={hexrays_sdk}")
+    subprocess.call(command)
+    subprocess.call(["cmake", "--build", ".", "--config", "Release", "--parallel"])
+
+
 cli.add_command(build_plugin)
 cli.add_command(build_loader)
+cli.add_command(build_everything)
 
 
 if __name__ == "__main__":
