aa83fce8ab6487f90a3b3c39d80a536730537a50
yeggor <yv@yegor.cc>
2025-01-19 04:09:05 +0000

    update copyright

--- efiXloader/uefitool.h
+++ efiXloader/uefitool.h
@@ -1,6 +1,6 @@
 /*
  * efiXloader
- * Copyright (C) 2020-2024 Binarly
+ * Copyright (C) 2020-2025 Binarly
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
